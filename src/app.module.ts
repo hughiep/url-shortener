b8516aa305e51701894904837fc7945ed1a553c8
Hiep Nguyen <hiepnv.wt@gmail.com>
2024-12-05 17:58:34 +0700

    use redis as store for cache manager

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ShortenerModule } from './modules/shortener/shortener.module';
 import { ConfigModule } from '@nestjs/config';
-import { CacheModule } from '@nestjs/cache-manager';
+import { CacheModule, CacheStore } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-store';
 
 @Module({
   imports: [
     ShortenerModule,
     ConfigModule.forRoot(),
-    CacheModule.register({
+    CacheModule.registerAsync({
       isGlobal: true,
+      useFactory: async () => {
+        const store = await redisStore({
+          password: process.env.REDIS_PASSWORD,
+        });
+
+        return {
+          store: store as unknown as CacheStore,
+          ttl: 60,
+        };
+      },
     }),
   ],
 })
